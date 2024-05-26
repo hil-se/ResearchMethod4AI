887acecd699c33acfe861675d41bed17d1579a38
azhe825 <zyu9@ncsu.edu>
2024-05-26 17:25:33 -0400

    test A2

--- test/test_A2.py
+++ test/test_A2.py
@@ -14,6 +14,6 @@ class TestA2(unittest.TestCase):
         np.testing.assert_array_equal(self.vgg.active_query(None, 3), np.array([2,5,3]))
 
     def test_active_query_5(self):
-        self.vgg.decision_function = Mock(return_value=np.array([0.81,0.83,0.56,0.31,0.12,0.67,0.1,0.2,1,0.12,0.9]))
-        self.vgg.predict = Mock(return_value=np.array([1,1,1,0,0,1,0,0,1,0,1]))
-        np.testing.assert_array_equal(self.vgg.active_query(None, 5), np.array([2,5,3,7,0]))
+        self.vgg.decision_function = Mock(return_value=np.array([0.81,0.83,0.56,0.31,0.12]))
+        self.vgg.predict = Mock(return_value=np.array([1,1,1,0,0]))
+        np.testing.assert_array_equal(self.vgg.active_query(None, 10), np.array([2,3,0,1,4]))
